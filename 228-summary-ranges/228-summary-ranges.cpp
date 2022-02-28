c5746fd53543e74e93c81164badf08a800ef3104
Amanjot Kaur <73784118+Amanjot-Kaur-Narang@users.noreply.github.com>
2022-02-28 16:45:57 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (62.02%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -9,17 +9,7 @@ public:
         ans.push_back(to_string(nums[0]));
         return ans;
       }
-       if(nums.size()==2){
-        if(nums[1]-nums[0]==1){
-        string s=to_string(nums[0])+"->"+ to_string(nums[1]);
-        ans.push_back(s);
-        return ans;
-      }
-         else{
-           ans.push_back(to_string(nums[0]));
-           ans.push_back(to_string(nums[1]));
-           return ans;
-         }}
+       
         for(int i=0;i<nums.size();i++){
           if(i==nums.size()-1 || (nums[i]+1)!=nums[i+1]){
             string s=to_string(nums[i]);
