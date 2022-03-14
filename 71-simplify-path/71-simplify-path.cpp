74fa1d33076e07ce0617546c455fa9de8a27f786
Amanjot Kaur <73784118+Amanjot-Kaur-Narang@users.noreply.github.com>
2022-03-14 17:53:48 +0530

    Time: 14 ms (21.68%), Space: 10.7 MB (25.77%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        int n=path.size();
+        stack<string> s;
+    
+      for(int i=0;i<n;i++){
+          string temp;
+        if(path[i]=='/'){
+          continue;
+        }
+        while(i<n && path[i]!='/'){
+          temp+=path[i++];
+          
+        }
+        if(temp==".."){
+          if(!s.empty()){
+            s.pop();
+          }
+        }
+        else if(temp=="."){
+          continue;
+        }
+        else{
+          s.push(temp);
+        }}
+      
+      string ans="";
+      while(!s.empty()){
+        ans='/'+s.top()+ans;
+        s.pop();
+        
+      }
+      if(ans=="") return "/";
+     
+        return ans;
+      
+    }
+};
