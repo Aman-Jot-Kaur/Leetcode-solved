7100a71c54d3348efd6d97b08032134bafa5d905
Amanjot Kaur <73784118+Amanjot-Kaur-Narang@users.noreply.github.com>
2022-08-01 23:33:55 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-  int countpaths(int i,int j,int m,int n,vector<vector<int>> &ans){
-    if(i==m-1 || j==n-1){
-      return 1;
-    }
-   else if(i>=m || j>=n){
-      return 0;
-    }
-    else if(ans[i][j]!=-1)
-      return ans[i][j];
-    else
-      return ans[i][j]=countpaths(i+1,j,m,n,ans)+countpaths(i,j+1,m,n,ans);
-      
-  }
     int uniquePaths(int m, int n) {
-       vector<vector<int>> ans(m+1,vector<int>(n+1,-1));
-       int answ=countpaths(0,0,m,n,ans);
-      return answ;
-    }
+        int turns=n+m-2;
+      int r=m-1;
+  double res=1;
+      for(int i=1;i<=r;i++){
+        res=res*(turns-r+i)/i;
+      }
+    return int(res);}
 };
